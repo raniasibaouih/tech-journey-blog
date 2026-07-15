ddd35081157b8a23d08be727369c98bb3d2c19b9
raniasibaouih <raniasibaouih@gmail.com>
2026-07-15 18:54:43 +0000

    tt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    content: [
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
